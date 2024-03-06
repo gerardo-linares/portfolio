193dda26a445532f6d5a43dbd7cd4b77a13fcbc5
gerardo linares <geracoder@gmail.com>
2024-03-06 05:18:59 -0300

    Add footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,21 @@
+import Socials from "./Socials";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-secondary py-12">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between">
+          <Socials
+            containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4"
+            iconsStyles="text-primary text-[20px] dark:text-white/70 hover:text-white dark:hover:text-primary transition-all"
+          />
+          <div className="text-muted-foreground ">
+            Copyright &copy; Gerardo Linares.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
